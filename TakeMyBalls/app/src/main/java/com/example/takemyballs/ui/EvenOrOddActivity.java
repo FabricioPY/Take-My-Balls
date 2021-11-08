0a6c214b678edb1e1a2d8d99b8c4da84c4f9d7b9
Fabricio Moura <fabricio92moura@gmail.com>
2021-11-08 11:50:36 -0300

    ponter clock

--- TakeMyBalls/app/src/main/java/com/example/takemyballs/ui/EvenOrOddActivity.java
+++ TakeMyBalls/app/src/main/java/com/example/takemyballs/ui/EvenOrOddActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -17,10 +19,26 @@ public class EvenOrOddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_even_or_odd);
 
+        ImageView pointer = findViewById(R.id.activity_poniter);
+        int topPointer = 0;
+
+        int time = 360;
+
+        float exoX = 100;
+        float exoY = 100;
+
         String even = "EVEN";
         String odd = "ODD";
 
         EvenOdd evenOdd;
+
+        Animation rotate = new RotateAnimation(topPointer, time, exoY, exoX);
+        rotate.setDuration(5000);
+        rotate.setFillAfter(true);
+        pointer.startAnimation(rotate);
+
+
+
 
         ImageView ButtonEven = findViewById(R.id.activity_button_even);
         ButtonEven.setOnClickListener(new View.OnClickListener() {
